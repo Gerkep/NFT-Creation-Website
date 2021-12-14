f472ca77d363732841737c7e5346bdcc3c1ed52a
Gerkep <piotrg2003@gmail.com>
2021-12-14 20:55:07 +0100

    main page design 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import '../style/App.css';
 
 const App = () =>{
     return(
-        <div>Hello my NFT world</div>
+        <div className="main-page">
+
+            <div className="navbar">
+                <p className="logo">NFTArt</p>
+                <div className="links">
+                    <a className="nav-link" href="#">About NFT's</a>
+                    <a className="nav-link" href="#">Getting started</a>
+                    <a className="nav-link" href="#">Pricing</a>
+                    <a className="nav-link" href="#">Consultation</a>
+                </div>
+                <button className="btn nav-btn">Get started!</button>
+            </div>
+
+            <div className="welcome-page">
+                <div className="welcome-text">
+                    <div className="slogan">
+                        <h1>Bring Your idea<br className="slogan-br"/> to <span className="colorful-text">NFT art.</span></h1>
+                        <h3 className="desktop-description">We gathered best designers to create beautiful art designed to become NFT.</h3>
+                        <button className="btn desktop-main-btn"></button>
+                    </div>
+                </div>
+                <div className="main-img">
+                    <div className="eth-illustration"></div>
+                </div>
+            </div>
+            <button className="btn mobile-main-btn">Get started!</button>
+
+        </div>
     )
 }
 
