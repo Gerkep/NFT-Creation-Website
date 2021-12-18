16755222b83d7fd43c4dee5ec625e74d816ec6a0
Gerkep <piotrg2003@gmail.com>
2021-12-18 17:55:27 +0100

    pricing done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../style/App.css';
+import OfferCard from "./OfferCard";
 
 const App = () =>{
     return(
         <div className="main-page">
 
             <div className="navbar">
-                <p className="logo">NFTArt</p>
+                <p className="logo">Nart</p>
                 <div className="links">
                     <a className="nav-link" href="#">About NFT's</a>
                     <a className="nav-link" href="#">Getting started</a>
@@ -20,8 +21,8 @@ const App = () =>{
                 <div className="welcome-text">
                     <div className="slogan">
                         <h1>Bring Your idea<br className="slogan-br"/> to <span className="colorful-text">NFT art.</span></h1>
-                        <h3 className="desktop-description">We gathered best designers to create beautiful art designed to become NFT.</h3>
-                        <button className="btn desktop-main-btn"></button>
+                        <h3 className="desktop-description">We gathered best designers to create<br/> beautiful art designed to become NFT.</h3>
+                        <button className="btn desktop-main-btn">Get started!</button>
                     </div>
                 </div>
                 <div className="main-img">
@@ -30,6 +31,15 @@ const App = () =>{
             </div>
             <button className="btn mobile-main-btn">Get started!</button>
 
+            <div className="section offer-section">
+                <h2 className="section-header">Choose project.</h2>
+                <OfferCard name="Basic" characters={1} charactersFormat="(.png,  .psd)" delivery={5} revisions={3} price={39.99}/>
+                <OfferCard name="Pro" characters="1K" delivery={8} revisions={5} price={299.99} pro={true}/>
+                <OfferCard name="Max" characters="10K" delivery={10} revisions={7} price={1239}/>
+            </div>
+            <div className="section about-section">
+                <h2 className="section-header">Explore NFT.</h2>
+            </div>
         </div>
     )
 }
---
+++ src/components/OfferCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../style/OfferCard.css';
+
+const OfferCard = ({name, characters, charactersFormat, delivery, revisions, price, pro}) => {
+
+    const isPro = () =>{
+        if(pro){
+            return "pro"
+        }
+    }
+    return(
+        <div>
+            <div className="offer-card">
+                <h2 className="name">{name}</h2>
+                <div className="detail number"><b className="colorful-text">{characters}</b> {characters === 1 ? "character" : "characters"} <span className="format">{charactersFormat}</span></div>
+               
+                <div className="detail delivery">Delivery in <b className="colorful-text">{delivery}</b> days</div>
+                <div className="detail revisions">Up to <b className="colorful-text">{revisions}</b> revisions*</div>
+                <div className="detail revisions"><b className="colorful-text">+</b>Video guide "how to mint NFT"</div>
+                <div className="detail price"><span className="from">from</span> ${price}</div>
+                <button className={`button ${isPro()}`}>Choose</button>
+                <p className="note">May vary depending on the complexity of the project.</p>
+            </div>
+        </div>
+    )
+}
+
+export default OfferCard;
