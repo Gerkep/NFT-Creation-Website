d36a039b059fdec710a7460490e5410468924e09
Gerkep <piotrg2003@gmail.com>
2022-01-16 14:22:53 +0100

    minor design changes

--- src/components/OfferCard.js
+++ src/components/OfferCard.js
@@ -24,7 +24,7 @@ const OfferCard = ({name, characters, charactersFormat, delivery, revisions, pri
                 <h2 className="name">{name}</h2>
                 <div className="detail number"><b className="colorful-text">{characters}</b> {characters === 1 ? "character" : "characters"} <span className="format">{charactersFormat}</span></div>
                
-                <div className="detail delivery">Delivery in <b className="colorful-text">{delivery}</b> days</div>
+                <div className="detail delivery">Delivery in <b className="colorful-text">{delivery}</b> days*</div>
                 <div className="detail revisions">Up to <b className="colorful-text">{revisions}</b> revisions*</div>
                 <div className="detail revisions"><b className="colorful-text">+</b>Video guide "how to mint NFT"</div>
                 <div className="detail price">${price}{costPerNFT()}</div>
--- src/components/ProjectSize.js
+++ src/components/ProjectSize.js
@@ -5,7 +5,7 @@ import OfferCard from "./OfferCard";
 const ProjectSize = () => {
     return(
         <div className="summary-background">
-            <Flow header="Choose project." link="/project/style">
+            <Flow header="Choose a project." link="/project/style">
             <OfferCard name="Basic" characters={1}ś delivery={5} revisions={2} price={49.99}/>
             <OfferCard name="Pro" characters="1K" delivery={8} revisions={4} price={1499}/>
             <OfferCard name="Max" characters="10K" delivery={14} revisions={6} price={3799} pro={true}/>
