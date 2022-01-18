f3708ca00a853944d7aa64721f5371b53fdc998f
Gerkep <piotrg2003@gmail.com>
2022-01-18 17:20:59 +0100

    sitemap added

---
+++ sitemap/sitemap-routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+        <Route path="/"/>
+        <Route path="/project"/>
+        <Route path="/project/style"/>
+        <Route path="/project/description"/>
+        <Route path="/project/summary"/>
+        <Route path="/success"/>
+        <Route path="/main"/>
+        <Route path="/main/contact"/>
+        <Route path="/main/contact/success" />
+    </Route>
+);
