19b6f5fe2b878a37d165ee754ed97cc04637c38e
Gerkep <piotrg2003@gmail.com>
2022-02-09 15:47:55 +0100

    twitter update

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -83,7 +83,7 @@ const MainPage = () => {
             <div className="social-media">
                     <a className="media-icon twitter" href="https://twitter.com/nftiece1"> </a>
                     <a className="media-icon instagram" href="https://www.instagram.com/nftiece/"> </a>
-                    <a className="media-icon tiktok" href="https://www.tiktok.com/@nftiece"> </a>
+                    <a className="media-icon tiktok" href="https://twitter.com/Nftiece_com"> </a>
             </div>
 
         </div>
